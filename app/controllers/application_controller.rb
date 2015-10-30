54a93fc93160f94ac822a97015a6325c364b3791
xiaocuixt <cjw624923@126.com>
2015-10-30 09:47:33 +0800

    config wechart environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :weixin_global
+  include ApplicationHelper
+
+  private
+  def weixin_global
+    unless $client.try(:is_valid?)
+      $client ||= WeixinAuthorize::Client.new(ENV["APPID"], ENV["APPSECRET"])
+    end
+  end
 end
