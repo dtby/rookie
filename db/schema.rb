ef7eda1761aef3198af48de5f5531c0215895167
OnlyCZY <853852998@qq.com>
2015-12-11 15:34:30 +0800

    fi

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209152752) do
-  
+ActiveRecord::Schema.define(version: 20151210072118) do
+
   create_table "applies", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "task_id",    limit: 4
