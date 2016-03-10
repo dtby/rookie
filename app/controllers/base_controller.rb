65a8e2f79e903837d1ed46187d262b27275e4862
zhangliugang <www.zhangyi508@gmail.com>
2016-03-10 10:35:28 +0800

    fix wechat

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -8,7 +8,16 @@ class BaseController < ApplicationController
   def weixin_global
     unless $client.try(:is_valid?)
       $client ||= WeixinAuthorize::Client.new(ENV["APPID"], ENV["APPSECRET"])
+    
+      $client.authorize_url(redirect_url)
+      # 回调后，获取code请求token或者openid:
+      sns_info = $client.get_oauth_access_token(params[:code])
+      # 再调用以下API，拉取用户信息：
+      user_info1 = $client.get_oauth_userinfo(openid, oauth_token)
     end
+
+    # pp user_info1
+    
   end
 
   def configure_permitted_parameters
